310b168ad5e17e4987645fe1b7a9504a8d635a6a
maarten1401 <maartendsi@hotmail.com>
2022-01-25 10:34:10 +0100

    got the games from api

---
+++ src/app/intercepters/http-headers.intercepter.ts
@@ -0,0 +1,26 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+      },
+      setParams: {
+        key: '8396f6c4ff66458b80a5f7a79949f8d9',
+      }
+    });
+    return next.handle(req);
+  }
+}
+
+
